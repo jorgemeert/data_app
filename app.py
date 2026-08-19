0e766ce4870283baaed41f5a3346cf218abd4a3d
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-08-19 13:04:02 -0300

    Falta só os ultimos ajustes
    vou deixar um pouco mais intuitivo, colocar o botão de baixar junto com o de converter e fazer tratamento de erros e exceções

--- app.py
+++ app.py
@@ -1,17 +1,64 @@
 import streamlit as st
 import pandas as pd
 import json
-import os
+from pathlib import Path
+
+
+def converter(arquivo,formato): 
+      if arquivo and formato:
+            nome = Path(arquivo.name.lower())
+            if nome.suffix == '.xlsx':
+                df = pd.read_excel(arquivo)
+            elif nome.suffix == '.csv':
+                df = pd.read_csv(arquivo)
+            elif nome.suffix == '.parquet':
+                df = pd.read_parquet(arquivo)
+            elif nome.suffix == '.json':
+                df = pd.read_json(arquivo)    
+
+            if formato == 'CSV':
+                df.to_csv(f'{nome.stem}.csv',index=False)
+                destino = f'{nome.stem}.csv'
+            elif formato == 'XLSX':
+                df.to_excel(f'{nome.stem}.xlsx', index=False)
+                destino = f'{nome.stem}.xlsx'
+            elif formato ==  'Parquet':
+                df.to_parquet(f'{nome.stem}.parquet')
+                destino = f'{nome.stem}.parquet'
+            else:
+                df.to_json(f'{nome.stem}.json')
+                destino = f'{nome.stem}.json'
+                    
+            with open(destino,'rb') as f:
+                data = f.read()
+
+            if '.csv' in destino:
+                mime = "text/csv"
+            elif '.xlsx' in destino:
+                mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            elif '.json' in destino:
+                mime = "application/json"    
+            else:
+                mime = "application/octet-stream"
+
+            st.download_button(
+                label="Baixar arquivo",
+                data=data,
+                file_name=destino,
+                mime= mime
+            )
+                   
 
 st.title("Bem vindo ao nosso conversor de arquivos")
 
-formato = st.selectbox("Digite o formato do arquivo que você deseja converter",['CSV','Parquet','XLSX','JSON'])
+formato = st.selectbox("Selecione o formato que deseja converter o arquivo",['CSV','Parquet','XLSX','JSON'])
 arquivo = st.file_uploader("Coloque aqui o arquivo que deseja converter")
 
 
 if st.button("Converter"):
     if arquivo and formato:
-        nome_arquivo = os.path.basename(arquivo)
-        if formato == 'CSV':
-            if '.xlsx' in nome_arquivo:
-                print('é de csv para xlxs')
+        converter(arquivo,formato)
+                
+
+ 
+    
